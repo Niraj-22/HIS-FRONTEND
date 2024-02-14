105aa6476d249709dbf799bccad5329882799a73
Niraj <niraj22chordia@gmail.com>
2024-02-14 14:33:17 +0530

    Changed Constant data

--- src/component/pages/Project.jsx
+++ src/component/pages/Project.jsx
@@ -3,6 +3,7 @@ import MultiSelectComponent from "./MultiSelectComponent";
 import Loader from "./Loader";
 import { filterData } from "../../utils/helperFunctions";
 import DisplaySelectedProject from "./DisplaySelectedProject";
+import { pjData } from "../../constant";
 const Project = () => {
   const [projectData, setProjectData] = useState([]);
   const [selectedProject, setSelectedProject] = useState([]);
@@ -11,85 +12,6 @@ const Project = () => {
   const [searchInput, setSearchInput] = useState();
   const [loading, setLoading] = useState(false);
   const [toggle, setToggle] = useState(false);
-  const pjData = [
-    {
-      projectId: 1,
-      projectName: "Project 1",
-      deadline: "2024-04-30",
-      description: "This is the first project description.",
-      clientName: "Client A",
-    },
-    {
-      projectId: 2,
-      projectName: "Project 2",
-      deadline: "2024-05-15",
-      description: "This is the second project description.",
-      clientName: "Client B",
-    },
-    {
-      projectId: 3,
-      projectName: "Project 3",
-      deadline: "2024-06-30",
-      description: "This is the third project description.",
-      clientName: "Client C",
-    },
-    {
-      projectId: 4,
-      projectName: "Project 4",
-      deadline: "2024-07-15",
-      description: "This is the fourth project description.",
-      clientName: "Client D",
-    },
-    {
-      projectId: 5,
-      projectName: "Project 5",
-      deadline: "2024-08-30",
-      description: "This is the fifth project description.",
-      clientName: "Client E",
-    },
-    {
-      projectId: 6,
-      projectName: "Project 6",
-      deadline: "2024-09-15",
-      description: "This is the sixth project description.",
-      clientName: "Client F",
-    },
-    {
-      projectId: 7,
-      projectName: "Project 7",
-      deadline: "2024-10-30",
-      description: "This is the seventh project description.",
-      clientName: "Client G",
-    },
-    {
-      projectId: 8,
-      projectName: "Project 8",
-      deadline: "2024-11-15",
-      description: "This is the eighth project description.",
-      clientName: "Client H",
-    },
-    {
-      projectId: 9,
-      projectName: "Project 9",
-      deadline: "2024-12-30",
-      description: "This is the ninth project description.",
-      clientName: "Client I",
-    },
-    {
-      projectId: 10,
-      projectName: "Project 10",
-      deadline: "2025-01-15",
-      description: "This is the tenth project description.",
-      clientName: "Client J",
-    },
-    {
-      projectId: 11,
-      projectName: "Project 11",
-      deadline: "2025-02-28",
-      description: "This is the eleventh description.",
-      clientName: "Client K",
-    },
-  ];
 
   useEffect(() => {
     const fetch = async () => {
