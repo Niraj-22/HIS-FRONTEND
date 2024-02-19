5a1faacadaeb23b78566c316a14f19bb4834ceef
Niraj <niraj22chordia@gmail.com>
2024-02-19 19:07:16 +0530

    Added Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,54 @@ import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { PrivateLayout, AdminLayout, PublicLayout } from "./component/layout";
-import {
-  AddDepartment,
-  AddUser,
-  AdminDashboard,
-  Home,
-  Login,
-  PageNotFound,
-  UpdateUser,
-  Profile,
-  AllUsers,
-  Project,
-} from "./component/pages";
+import { Suspense, lazy } from "react";
+import Loader from "./component/pages/Loader";
+// import {
+//   AddDepartment,
+//   AddUser,
+//   AdminDashboard,
+//   Home,
+//   Login,
+//   PageNotFound,
+//   UpdateUser,
+//   Profile,
+//   AllUsers,
+//   Project,
+// } from "./component/pages";
+const AddDepartment = lazy(() => import("./component/pages/AddDepartment"));
+const AddUser = lazy(() => import("./component/pages/AddUser"));
+const AdminDashboard = lazy(() => import("./component/pages/AdminDashboard"));
+const Home = lazy(() => import("./component/pages/Home"));
+const Login = lazy(() => import("./component/pages/Login"));
+const PageNotFound = lazy(() => import("./component/pages/PageNotFound"));
+const UpdateUser = lazy(() => import("./component/pages/UpdateUser"));
+const Profile = lazy(() => import("./component/pages/Profile"));
+const AllUsers = lazy(() => import("./component/pages/AllUsers"));
+const Project = lazy(() => import("./component/pages/Project"));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<PrivateLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/update-user" element={<UpdateUser />} />
-          <Route path="/all-users" element={<AllUsers />} />
-          <Route path="/project" element={<Project />} />
-        </Route>
-        <Route element={<AdminLayout />}>
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/add-user" element={<AddUser />} />
-          <Route path="/add-dept" element={<AddDepartment />} />
-        </Route>
-        <Route element={<PublicLayout />}>
-          <Route path="login" element={<Login />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route element={<PrivateLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/update-user" element={<UpdateUser />} />
+            <Route path="/all-users" element={<AllUsers />} />
+            <Route path="/project" element={<Project />} />
+          </Route>
+          <Route element={<AdminLayout />}>
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/add-user" element={<AddUser />} />
+            <Route path="/add-dept" element={<AddDepartment />} />
+          </Route>
+          <Route element={<PublicLayout />}>
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
       <ToastContainer />
     </>
   );
--- src/component/pages/Loader.jsx
+++ src/component/pages/Loader.jsx
@@ -3,7 +3,7 @@ import { BeatLoader } from "react-spinners";
 
 const Loader = ({ loading }) => {
   return (
-    <div className="flex justify-center p-8">
+    <div className="flex justify-center p-8 h-screen items-center w-full bg-zinc-800">
       <BeatLoader color={"#FFFFFF"} loading={loading} size={15} />
     </div>
   );
